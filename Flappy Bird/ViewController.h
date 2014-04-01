847323e58320b0ddcf7410cb38b9a8afc07e55cb
Yuji Shimizu <yuji.shimizu@dena.com>
2014-03-31 21:55:30 -0700

    Initial Commit

---
+++ Flappy Bird/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Flappy Bird
+//
+//  Created by Yuji Shimizu on 3/31/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Flappy BirdTests/Flappy_BirdTests.m
@@ -0,0 +1,34 @@
+//
+//  Flappy_BirdTests.m
+//  Flappy BirdTests
+//
+//  Created by Yuji Shimizu on 3/31/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flappy_BirdTests : XCTestCase
+
+@end
+
+@implementation Flappy_BirdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
